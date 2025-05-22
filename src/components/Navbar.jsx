31581cdb04bd6cc5b57ecc02272f79f6cd69ab09
Subhradeep1708 <subhradeep1708@gmail.com>
2025-05-23 00:56:03 +0530

    Add Navbar component with logo placeholder

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className=''>
+            {/* LOGO */}
+            <div className="">
+                <img src="./logo.png" alt="Logo" className='w-8 h-8' />
+            </div>
+            {/* Mobile */}
+            {/* Desktop */}
+        </div>
+    )
+}
+
+export default Navbar
