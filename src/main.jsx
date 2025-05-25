eed2884f02081087ddf87a3e6024bd786ccd0244
Subhradeep1708 <subhradeep1708@gmail.com>
2025-05-26 00:54:16 +0530

    Refactor project structure: rename app, update dependencies, and add routing components

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './routes/Homepage.jsx'
+const router = createBrowserRouter([
+  { path: "/", Component: <Homepage /> },
+  { path: "/", Component: Root },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </StrictMode>,
 )
