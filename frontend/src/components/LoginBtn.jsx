cf8f48ffc5e341519f3b7f2282df2f475c4f0c27
Subhradeep1708 <subhradeep1708@gmail.com>
2025-08-21 02:02:06 +0530

    fix minor navbar issues

---
+++ frontend/src/components/LoginBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginBtn = () => {
+    return (
+        <div>
+            <a href="/">
+                <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white hover:bg-blue-600'>Login 👋</button>
+            </a>
+        </div>
+    )
+}
+
+export default LoginBtn
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import { Image } from '@imagekit/react'
-import React, { useState } from 'react'
+import { useState } from 'react'
+import { RxCross1, RxHamburgerMenu } from "react-icons/rx";
+import LoginBtn from './LoginBtn';
+
 
 const Navbar = () => {
+    const navbarLinks = [
+        { id: 1, name: "Home", href: "/" },
+        { id: 2, name: "Trending", href: "/" },
+        { id: 3, name: "Most Popular", href: "/" },
+        { id: 4, name: "About", href: "/" },
+    ]
+
     const [open, setOpen] = useState(false)
     return (
-        <div className='w-full md:h-20 flex items-center justify-between '>
+        <div className='w-full md:h-20 flex items-center justify-between px-8 py-8 backdrop-blur-md'>
             {/* LOGO */}
             <div className=" flex items-center gap-4 font-bold text-2xl ">
                 <img src="./logo2.svg" alt="Logo" className='w-9 h-9' />
@@ -19,31 +29,48 @@ const Navbar = () => {
             </div>
             {/* Mobile Menu*/}
             <div className="md:hidden">
-                <div className="cursor-pointer text-4xl" onClick={() => setOpen((prev) => !prev)}>
-                    {open ? "X" : "☰"}
+                {/* <div className="cursor-pointer text-4xl" onClick={() => setOpen((prev) => !prev)}>
+                    {open ? <RxCross1 /> : <RxHamburgerMenu />
+}
+                </div> */}
+                <div
+                    className="cursor-pointer relative w-10 h-10 flex items-center justify-center"
+                    onClick={() => setOpen((prev) => !prev)}
+                >
+                    <RxHamburgerMenu
+                        className={`absolute text-4xl transform transition-all duration-500 ${open ? "opacity-0 rotate-90 scale-50" : "opacity-100 rotate-0 scale-100"
+                            }`}
+                    />
+                    <RxCross1
+                        className={`absolute text-4xl transform transition-all duration-500 ${open ? "opacity-100 rotate-0 scale-100" : "opacity-0 -rotate-90 scale-50"
+                            }`}
+                    />
                 </div>
-                {/* Mobile Links List */}
-                <div className={`w-full h-screen bg-[#e6e6ff] flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${open ? "-right-0" : "-right-[100%]"
-                    }`}>
 
-                    <a href="/">Home</a>
+                {/* Mobile Links List */}
+                <div className={`w-full h-screen bg-[#e6e6ff] items-center justify-center gap-8 font-medium text-lg absolute top-18 transition-all ease-in-out duration-700 ${open ? "right-0 flex flex-col  " : "opacity-0 hidden"} backdrop-blur-3xl `}>
+                    {navbarLinks.map((id, name, href) => (
+                        <a href={href} key={id}>{name}</a>
+                    ))}
+                    {/* <a href="/">Home</a>
                     <a href="/">Trending</a>
                     <a href="/">Most Popular</a>
-                    <a href="/">About</a>
-                    <a href="/">
-                        <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login 👋</button>
-                    </a>
+                    <a href="/">About</a> */}
+                    <LoginBtn />
                 </div>
             </div>
+
             {/* Desktop Menu*/}
-            <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-                <a href="/">Home</a>
+            <div className="hidden md:flex items-center gap-8 xl:gap-12 font-semibold ">
+                {/* <a href="/" className='hover:underline duration-900 transform  transition-transform'>Home</a> */}
+                {navbarLinks.map((nav) => (
+                    <a href={nav.href} key={nav.id} className='duration-700 transform  transition-transform hover:scale-110'>{nav.name}</a>
+                ))}
+                {/* <a href="/">Home</a>
                 <a href="/">Trending</a>
                 <a href="/">Most Popular</a>
-                <a href="/">About</a>
-                <a href="/">
-                    <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login 👋</button>
-                </a>
+                <a href="/">About</a> */}
+                <LoginBtn />
             </div>
         </div>
     )
