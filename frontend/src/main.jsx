1846503a01c53613b9ceb5d0d4394a4599652651
Subhradeep1708 <subhradeep1708@gmail.com>
2025-08-21 00:30:50 +0530

    feat: initialize frontend with Vite, React, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Configured PostCSS with Tailwind CSS
    - Created SVG icons for delete, Facebook, Instagram, and logo
    - Added featured images for posts
    - Implemented main App component and Navbar
    - Developed Image component for optimized image loading
    - Set up routing with React Router for Homepage, Login, Post List, Single Post, and Write pages
    - Added global styles with Tailwind CSS
    - Configured Vite for development and build processes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Homepage from './routes/Homepage.jsx'
+import Navbar from './components/Navbar.jsx'
 const router = createBrowserRouter([
-  { path: "/", Component: <Homepage /> },
-  { path: "/", Component: Root },
+  { path: "/", Component: Homepage },
+  // { path: "/", Component: Root },
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}>
+      <Navbar />
       <App />
     </RouterProvider>
   </StrictMode>,
